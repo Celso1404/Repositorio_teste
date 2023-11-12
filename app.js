21eca415bb3914a5d085264fab89878c2990693b
Celso1404 <celsokaiiki@gmail.com>
2023-11-12 19:14:44 -0300

    Voltando em uma versão anterior do arquivo

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Analisando o sistema");
-
-//Mais um console de teste
-console.log("O Celso está testando o commit do git");
