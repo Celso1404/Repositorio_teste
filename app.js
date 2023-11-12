e37f2a2d932cd2ee0a80784d7778dc66df0c5c61
Celso Kaiki <98421039+Celso1404@users.noreply.github.com>
2023-11-12 18:59:47 -0300

    Atualizando o arquivo

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Analisando o sistema");
+
+//Mais um console de teste
+console.log("O Celso está testando o commit do git");
